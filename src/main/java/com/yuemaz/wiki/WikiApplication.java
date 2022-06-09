cd86de718c9abdb420c538e5b338ed74959afcdb
yuemaQaQ <mayuezzz@163.com>
2022-06-09 23:29:06 +0800

    2.4 启动类信息修改

--- src/main/java/com/yuemaz/wiki/WikiApplication.java
+++ src/main/java/com/yuemaz/wiki/WikiApplication.java
@@ -1,13 +1,22 @@
 package com.yuemaz.wiki;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
+import java.net.Inet4Address;
+import java.net.UnknownHostException;
+
+@Slf4j
 @SpringBootApplication
 public class WikiApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(WikiApplication.class, args);
+    public static void main(String[] args) throws UnknownHostException {
+        SpringApplication app = new SpringApplication(WikiApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        log.info("启动成功！！");
+        log.info("地址: \thttp://" + Inet4Address.getLocalHost().getHostAddress() + ":{}", env.getProperty("server.port"));
     }
 
 }

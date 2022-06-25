3d50a15b0a5852a2909e83e5e0f611b7bcdcca70
yuemaQaQ <mayuezzz@163.com>
2022-06-26 00:57:04 +0800

    5.2 前后端跨域

---
+++ src/main/java/com/yuemaz/wiki/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.yuemaz.wiki.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+/**
+ * @Author yuemaz
+ * @Date 2022/6/26 0:54
+ */
+@Configuration
+public class CorsConfig extends WebMvcConfigurationSupport {
+
+    @Override
+    protected void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedHeaders(CorsConfiguration.ALL)
+                .allowedMethods(CorsConfiguration.ALL)
+                .allowCredentials(true)
+                .maxAge(3600); // 1小时内不需要再预检（发OPTIONS请求）
+    }
+
+}
